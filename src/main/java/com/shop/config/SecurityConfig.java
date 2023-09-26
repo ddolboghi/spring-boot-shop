943ffa52e4c2387be3794b845fe71b32b63a7bae
ddolboghi <rhdfyd128@gmail.com>
2023-09-26 16:00:58 +0900

    spring security 6.1.4 적용

--- src/main/java/com/shop/config/SecurityConfig.java
+++ src/main/java/com/shop/config/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.shop.config;
 
-import com.shop.service.MemberService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,27 +13,48 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    MemberService memberService;
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//            .authorizeHttpRequests(authorize -> authorize
-//                    .requestMatchers("/members/login").permitAll()
-//                    .anyRequest().authenticated()
-//            )
-//            .formLogin(formLogin -> formLogin
-//                    .loginPage("/login")
-//                    .permitAll()
-//            )
-//            .rememberMe(Customizer.withDefaults());
-
-        return http.build();
+        return http
+                // Here we are configuring our login form
+                .formLogin(formLogin -> {
+                            formLogin
+                                    .loginPage("/members/login") // Login page will be accessed through this endpoint. We will create a controller method for this.
+                                    .usernameParameter("email")
+                                    .passwordParameter("password")
+                                    .permitAll() // We re permitting all for login page
+                                    .defaultSuccessUrl("/") // If the login is successful, user will be redirected to this URL.
+                                    .failureUrl("/members/login/error"); // If the user fails to login, application will redirect the user to this endpoint
+                        }
+                )
+                .authorizeHttpRequests(authorize -> {
+                    authorize
+                            .requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/img/**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/members/**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/item/**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll()
+                            .requestMatchers(new AntPathRequestMatcher("/admin/**")).hasRole("ADMIN")
+                            .anyRequest().authenticated();
+                    }
+                )
+                .logout(logout ->
+                        logout
+                                .logoutRequestMatcher(new AntPathRequestMatcher("/members/logout"))
+                                .logoutSuccessUrl("/")
+                )
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                )
+                .build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -10,10 +10,13 @@ import org.springframework.transaction.annotation.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MemberService implements UserDetailsService {
+
     private final MemberRepository memberRepository;
 
     public Member saveMember(Member member) {
@@ -21,17 +24,19 @@ public class MemberService implements UserDetailsService {
         return memberRepository.save(member);
     }
 
-    private void validateDuplicateMember(Member member) {
+    private void validateDuplicateMember(Member member){
         Member findMember = memberRepository.findByEmail(member.getEmail());
-        if(findMember != null) {
+        if(findMember != null){
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
     }
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
         Member member = memberRepository.findByEmail(email);
-        if(member == null) {
+
+        if(member == null){
             throw new UsernameNotFoundException(email);
         }
 
